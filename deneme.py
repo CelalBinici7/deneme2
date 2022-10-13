8ea8f8b984206bd79554d22cc6ce7238e82c103e
cll <61542865+CelalBinici7@users.noreply.github.com>
2022-10-13 17:12:49 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,1 +1,16 @@
 print "Merhaba , Yeniden Merhaba"
+
+ public void insertion_sort(int[] dizi) 
+   {
+     for(int j = 1; j < dizi.Length; j++)
+     {
+        int key = dizi[j]; 
+        int i = j - 1;  
+        while (i >= 0 && dizi[i] > key) 
+        {
+          dizi[i + 1] = dizi[i]; 
+           i = i - 1;  
+        }
+        dizi[i + 1] = key;
+      }
+    }
