93c7cb7a0c3238fbb6e044ab1481cad4c776e784
CelalBinici7 <celalbinici150@gmail.com>
2022-12-04 22:10:50 +0300

    deneme.py e kod eklendi

--- deneme.py
+++ deneme.py
@@ -14,3 +14,18 @@ print "Merhaba , Yeniden Merhaba"
         dizi[i + 1] = key;
       }
     }
+    
+     public void bubble_sort(int[] dizi) 
+   {
+     for(int j = 1; j < dizi.Length; j++)
+     {
+        int key = dizi[j]; 
+        int i = j - 1;  
+        while (i >= 0 && dizi[i] > key) 
+        {
+          dizi[i + 1] = dizi[i]; 
+           i = i - 1;  
+        }
+        dizi[i + 1] = key;
+      }
+    }
